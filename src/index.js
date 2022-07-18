b47575ca104c713a57685d10bebc4cfe761c9291
Pauli Jokela <pauli.jokela@didstopia.com>
2022-07-18 15:47:24 +0300

    Package module and building fixes, new RC

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var config_1 = require("./config");
-var platform_1 = require("./platform");
-module.exports = function (api) {
-    api.registerPlatform(config_1.PLUGIN_NAME, config_1.PLATFORM_NAME, platform_1["default"]);
-};
